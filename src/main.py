b5c7da7f6ce30c999c9fb3fac88eac7aa4e84cce
kh2ffx <kh2ffx@gmail.com>
2025-04-06 19:24:04 -0500

    4-6-25(2)

--- src/main.py
+++ src/main.py
@@ -1,17 +1,16 @@
 from textnode import TextNode, TextType
 from copypaste import copy_paste
-from pagegen import generate_page
+from pagegen import generate_page, recursive_pagegen
 
 def main():
     #copy_paste will need to be updated to handle different filepaths
     source = "static"
     destination = "public"
     copy_paste(source, destination)
-    generate_page(f"content/index.md", "template.html", f"{destination}/index.html")
-    generate_page(f"content/blog/glorfindel/index.md", "template.html", "public/blog/glorfindel/index.html")
-    generate_page(f"content/blog/majesty/index.md", "template.html", "public/blog/majesty/index.html")
-    generate_page(f"content/blog/tom/index.md", "template.html", "public/blog/tom/index.html")
-    generate_page(f"content/contact/index.md", "template.html", "public/contact/index.html")
+    generate_page(f"content/index.md", 
+                  "template.html", 
+                  f"{destination}/index.html")
+    recursive_pagegen("content", "template.html", "public")
 
 
 main()
--- src/pagegen.py
+++ src/pagegen.py
@@ -1,6 +1,7 @@
 import os
 from blocks import markdown_to_html_node, extract_title
 from htmlnode import LeafNode 
+from copypaste import copy_tree, list_paths
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
@@ -14,7 +15,6 @@ def generate_page(from_path, template_path, dest_path):
     #close
     from_file_ptr.close()
     template_file_ptr.close()
-
     title = extract_title(from_file)
 
     from_file = markdown_to_html_node(from_file)
@@ -23,17 +23,15 @@ def generate_page(from_path, template_path, dest_path):
     template_file_two = template_file.replace("{{ Title }}", title)
     final_file = template_file_two.replace("{{ Content }}", file_contents)
     
+    # I hate the whole idea of this and I've probably done it in a stupid way
+    # I will fix this one day (probably not)
     destination = dest_path.split("/")
     folder_count = 0
     final_destination = "" 
-    print(0000, destination)
     while folder_count < len(destination) - 1:
-        print(9999, destination[folder_count], final_destination)
         if not os.path.exists(f"{destination[folder_count]}") and folder_count == 0:
-            print(1111)
             os.mkdir(f"destination[0]")
         if not os.path.exists(f"{final_destination}/{destination[folder_count]}") and folder_count > 0:
-            print(2222, final_destination, destination[folder_count])
             os.mkdir(f"{final_destination}/{destination[folder_count]}")
         final_destination += f"{destination[folder_count]}/"
         folder_count += 1
@@ -43,4 +41,24 @@ def generate_page(from_path, template_path, dest_path):
     dest_file_ptr.write(final_file)
     dest_file_ptr.close()
 
+# adding this here, but I should replace a function in copypaste with this one
+def recursive_pagegen_helper(directory, template_path):
+    for file in os.listdir(directory):
+        filepath = os.path.join(directory, file)
+        if os.path.isfile(filepath) and file[len(file) - 3 :] == ".md":
+            generate_page(filepath, template_path, 
+            f"{filepath[:len(filepath) - 3]}.html")
+            #delete .md file later
+        elif not os.path.isfile(filepath):
+            recursive_pagegen_helper(filepath, template_path)
 
+def recursive_pagegen(from_path_directory, 
+                      template_path, 
+                      destination_path_directory):
+    # copy everything from content directory to public 
+    copy_tree(list_paths(from_path_directory), 
+              from_path_directory, 
+              destination_path_directory)
+    # Now I need to convert the markdown ".md" files in the public
+    # directory to html 
+    recursive_pagegen_helper(destination_path_directory, template_path)
